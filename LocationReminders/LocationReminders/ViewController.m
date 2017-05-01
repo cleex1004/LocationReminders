13f6be20d97d91a499ff7fe6f0072f90eb195e36
cleex1004 <cleex1004@gmail.com>
2017-05-01 12:41:19 -0700

    end of lecture

--- LocationReminders/LocationReminders/ViewController.m
+++ LocationReminders/LocationReminders/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+@import Parse;
+@import MapKit;
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -16,9 +22,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self requestPermissions];
+    self.mapView.showsUserLocation = YES;
 }
 
+-(void)requestPermissions{
+    self.locationManager = [[CLLocationManager alloc]init];
+    [self.locationManager requestAlwaysAuthorization];
+    
+}
+
+- (IBAction)location1Pressed:(id)sender {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6566674, -122.351096);
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+    
+    [self.mapView setRegion:region animated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -27,3 +47,23 @@
 
 
 @end
+//TEST CODE
+//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+//
+//    testObject[@"testName"] = @"Adam Wallraff";
+//    [testObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+//        if (succeeded) {
+//            NSLog(@"Success saving test object!");
+//        } else {
+//            NSLog(@"There was a problem saving. Save Error: %@", error.localizedDescription);
+//        }
+//    }];
+//
+//    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
+//    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"%@", error.localizedDescription);
+//        } else {
+//            NSLog(@"Query Results %@", objects);
+//        }
+//    }];
